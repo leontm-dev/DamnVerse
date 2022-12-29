513212e289940efb6340274f930494e757135cc3
Leon <LAnneken09@gmail.com>
2022-12-29 21:05:52 +0100

    DamnChaotix YouTube Public API

---
+++ Routen/Allgemein/YouTube/youtube.js
@@ -0,0 +1,10 @@
+//Datei-Einstellungen
+const express = require("express");
+const db = require("easy-db-json");
+const router = express.Router();
+//Datenbank
+
+//Routen
+
+//Exports
+module.exports = router;
